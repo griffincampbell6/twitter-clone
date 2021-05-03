a3a4e7de8759414624ff3958987b7be572ad66cd
griffincampbell6 <griffincampbell6@gmail.com>
2021-05-03 11:42:20 -0400

    amplify auth imports

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Amplify from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react-native';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import config from './src/aws-exports';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
